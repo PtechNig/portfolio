8b6076cad610460b577fdcb86d1c3d00c9d4991e
PtechNig <oyelamioluwaseunp@gmail.com>
2024-11-27 17:27:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Hero from './components/Hero/Hero'
+import Nav from './components/Nav/Nav'
+import Skill from './components/Skills/Skill'
+import Work from './components/Work/Work'
+import Footer from './components/Footer/Footer'
+
+function App() {
+
+  return (
+    <>
+    <Nav/>
+    <Hero/>
+    <About/>
+    <Skill/>
+    <Work/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
