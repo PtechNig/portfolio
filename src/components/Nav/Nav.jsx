8a2ad6d04e0a17ee6813de1a9e5b738e2ec07396
PtechNig <oyelamioluwaseunp@gmail.com>
2024-12-16 13:14:24 +0100

    hamberger functionality is added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,36 @@
 import "./Nav.css"
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { IoClose } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
 const navbar = () => {
+
+  let [isOpen, setIsOpen] = useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false)
+  }
   return (
     <div className='navbar_container'>
       <div className='logo'>
        <img src='/assets/group-logo.png' alt='' className='logo'/>
       </div>
-          <ul>
-            <li> <AnchorLink  href="#hero"> HOME </AnchorLink>  </li>
-            <li> <AnchorLink offset={40} href="#about">ABOUT</AnchorLink> </li>
-            <li> <AnchorLink offset={100} href="#skill">SKILLS</AnchorLink> </li>
-            <li> <AnchorLink offset={70} href="#work">WORK</AnchorLink> </li>
-            <li> <AnchorLink offset={50} href="#contact">CONTACT</AnchorLink> </li>
+      
+      <div className="nav-icon" onClick={toggle}>
+        {isOpen ? <IoClose  /> : <GiHamburgerMenu  />}
+      </div>
+          <ul className= {`ul ${isOpen ? "open" : " "}`}>
+          <div className="close">
+          </div>
+            <li> <AnchorLink  href="#hero" onClick={closeMenu}> HOME </AnchorLink>  </li>
+            <li> <AnchorLink offset={40} href="#about" onClick={closeMenu}>ABOUT</AnchorLink> </li>
+            <li> <AnchorLink offset={100} href="#skill" onClick={closeMenu}>SKILLS</AnchorLink> </li>
+            <li> <AnchorLink offset={70} href="#work" onClick={closeMenu}>WORK</AnchorLink> </li>
+            <li> <AnchorLink offset={50} href="#contact" onClick={closeMenu}>CONTACT</AnchorLink> </li>
 
           </ul>
     </div>
